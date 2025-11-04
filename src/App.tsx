b6e50f3c58caea9c0dc7c274bc1d0df733541174
Himanshu <149322620+himanshu1081@users.noreply.github.com>
2025-11-05 05:19:19 +0530

    folder name changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
-import { Hero } from './components/Hero'
-import Introduction from './components/Introduction'
-import Services from './components/Techstack'
-import Projects from './components/Projects'
+import { Hero } from './sections/Hero'
+import Introduction from './sections/Introduction'
+import Services from './sections/Techstack'
+import Projects from './sections/Projects'
 import Lenis from 'lenis'
-import Outro from './components/Outro'
+import Outro from './sections/Outro'
 
 
 
