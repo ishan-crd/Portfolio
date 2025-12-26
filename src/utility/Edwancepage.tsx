e48a67930edcd517d986026e3bbfc4142c81a89a
unknown <ishangupta3121@gmail.com>
2025-12-27 01:24:49 +0530

    updated colour in edwancepage

--- src/utility/Edwancepage.tsx
+++ src/utility/Edwancepage.tsx
@@ -2,7 +2,7 @@ const Edwancepage: React.FC = () => {
   return (
     <div className="relative w-full mt-30 h-60 mb-70 flex justify-center items-center">
       {/* Background blur */}
-      <div className="absolute inset-0 bg-blue-950/60 blur-3xl h-60 z-0" />
+      <div className="absolute inset-0 bg-orange-500/45 blur-3xl h-60 z-0" />
 
       {/* Image */}
       <div className="absolute inset-0 flex justify-center items-center z-10">
